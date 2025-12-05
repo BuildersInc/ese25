51be18038cef7d07033c459d75c36e3646124a1a
FelixSchmidt <Felix-Schmidt2@outlook.de>
2025-12-05 11:02:06 +0100

    Aufgabe 2 Abgeschlossen

--- aufgabe_2/rgb_brd.c
+++ aufgabe_2/rgb_brd.c
@@ -1,24 +1,30 @@
 /* Required Header */
 #include <linux/i2c.h>
+#include <linux/init.h>
+#include <linux/module.h>
 
 /* Benenne alle kompatiblen Geräte */
 static struct i2c_device_id my_ids[] = {
-	/* TODO: Füge das kompatible Gerät ein*/
+	/* TODO: Fge das kompatible Gerät ein*/
+	{"rgb_brd"},
 	{} /* leeres Element signalisiert das Ende der Liste */
 };
-MODULE_DEVICE_TABLE(i2c, my_ids);
+MODULE_DEVICE_TABLE(i2c, my_ids); // important for the autoloading
+// nur dann wichtig wenn ich mit in den Kernel reinkompilen will
 
-/* Funktion wird aufgerufen , wenn ein kompatibles I2C Gerät hinzugefügt wird */
+/* Funktion wird aufgerufen, wenn ein kompatibles I2C Geraet hinzugefuegt wird */
 static int my_probe(struct i2c_client *client)
 {
 	/* TODO: Füge den benötigten Code ein... */
+	printk("Hallo vom I2C Slave mit der Adresse: 0x%x\n", client->addr);
 	return 0;
 }
 
 /* Funktion wird aufgerufen , wenn ein kompatibles I2C Gerät entfernt wird*/
 static void my_remove(struct i2c_client *client)
 {
-	/* TODO: Füge den benötigten Code ein... */
+	/* TODO: Fgedenbentigten Code ein... */
+	printk("Bye bye 0x%x\n", client->addr);
 }
 
 /* Fasse kompatible Geräte , Probe - und Remove - Funktionen in Treiber zusammen */
@@ -31,10 +37,37 @@ static struct i2c_driver my_driver = {
 	}
 };
 
+
+static int __init my_driver_init(void)
+{
+	int ret = 0;
+	ret = i2c_add_driver(&my_driver);
+	printk("I2C Driver returned %i\n", ret);
+
+	return ret;
+}
+
+static void my_driver_exit(void)
+{
+	printk("Bye Bye I2C");
+	i2c_del_driver(&my_driver);
+}
+
+// Alternativ
+// module_i2c_driver(my_driver);
+
+// Adden des devices
+// cd /sys/bus/i2c/devices/i2c-1
+// ADD: 	echo "rbg_brd 0x20 " | sudo tee new_devices
+// REMOVE	echo "0x20 " | sudo tee delete_device
+
 /* Registriere den Treiber beim OS */
-module_i2c_driver(my_driver);
+// module_i2c_driver(my_driver);
 
 /* Informationen über den Treiber */
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR(""); /* TODO: Namen einfügen*/
-MODULE_DESCRIPTION(""); /* TODO: Beschreibung einfügen */
+MODULE_AUTHOR("Felix Schmidt");
+MODULE_DESCRIPTION("Basic I2C Treiber");
+
+module_init(my_driver_init);
+module_exit(my_driver_exit);
